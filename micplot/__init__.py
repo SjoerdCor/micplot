67f33bd86dcc73d1416da938c41d74b3aa4df24d
Sjoerd Cor <sjoerdcornelissen@gmail.com>
2020-10-03 17:49:18 +0200

    Add documentation

--- micplot/__init__.py
+++ micplot/__init__.py
@@ -1,1 +1,2 @@
-__version__ = "1.0.0"
+__version__ = "0.0.1"
+from visualization import Consultant
--- micplot/visualization.py
+++ micplot/visualization.py
@@ -4,17 +4,22 @@ import pandas as pd
 import numpy as np
 import math
 import seaborn as sns
+import sklearn.preprocessing
 
 class Consultant:
+    '''
+    A helper who outputs nice visualizations when given data
+    '''
     
     def __init__(self, highlight_color='purple'):
         '''
-        TODO: write docstring
+        Parameters
+        ----------
+        highlight_color: the color to highlight interesting bars
         '''
         self.highlight_color = highlight_color
-        self.contrasting_text_color = _return_contrasting_text_color(self.highlight_color)
+        self.contrasting_text_color = self._return_contrasting_text_color(self.highlight_color)
         
-    #TODO: method does not use self; look into this
     def recommmend_plottype(self, data):
         '''
         Determines plottype base on shape and content of data
@@ -34,24 +39,31 @@ class Consultant:
             else:
                 plottype = 'line_timeseries'
         elif isinstance(data, pd.Series):
-            if is_percentage_series(data):
+            if self._is_percentage_series(data):
                 plottype = 'waterfall'
             else:
                 plottype = 'bar'
         elif isinstance(data, pd.DataFrame):
-            if data.apply(is_percentage_series).all():
+            if data.apply(self._is_percentage_series).all():
                 plottype = 'composition_comparison'
             else:
                 plottype = 'scatter'
         return plottype
     
     def determine_stringformat(self, data):
-        #TODO: Write docstring
-        #TODO: take datatype into account
-        #TODO: Shouldn't we check that the data to be plotted is numerical?
-        if (isinstance(data, pd.DataFrame) and data.apply(is_percentage_series).all()) or\
-            (isinstance(data, pd.Series) and is_percentage_series(data)):
+        '''
+        Determine label precision from data type
+        
+        Parameters
+        ----------
+        data : pandas Dataframe or Series with data to label
+        '''
+        if (isinstance(data, pd.DataFrame) and data.apply(self._is_percentage_series).all()) or\
+            (isinstance(data, pd.Series) and self._is_percentage_series(data)):
             strfmt = '.1%'
+        elif (isinstance(data, pd.DataFrame) and data.apply(pd.api.types.is_integer_dtype).all()) or\
+            (isinstance(data, pd.Series) and pd.api.types.is_integer_dtype(data)):
+            strfmt = 'd'
         else:
             strfmt = '.2f'
         return strfmt
@@ -80,17 +92,14 @@ class Consultant:
                 color = [cmap(x) for x in np.linspace(1/lendata, 1, lendata)]
         else:
             raise TypeError('data should be of type DataFrame or Series')
-
-        try: #TODO: Why use error catching and not just make everything into a list?
-            for h in highlight:
-                if h < 0: # Count from the back
-                    h = len(data) + h
-                color[h] = self.highlight_color
-        except TypeError:
-            color[highlight] = self.highlight_color
+        
+        if isinstance(highlight, int):
+            highlight = [highlight]
+        for h in highlight:
+            if h < 0: # Count from the back
+                h += len(data)
+            color[h] = self.highlight_color
         return color
-
-
     
     def visualize(self, data, highlight=-1, plottype=None, ascending=True, strfmt=None, **kwargs):
         '''
@@ -117,7 +126,7 @@ class Consultant:
         '''
         if not isinstance(data, pd.DataFrame) and not isinstance(data, pd.Series):
             raise TypeError(f'Data is not of type Series or DataFrame, but type {type(data)}')
-        #TODO: Check that data is numerical
+        #TODO: Check that data is numeric
         
         #PREPARING THE PLOT        
         data = data.squeeze() # DataFrame with single column should be treated as Series
@@ -143,42 +152,43 @@ class Consultant:
         if plottype == 'bar':
             ax = data.plot(kind='barh', color=color, **kwargs)
             if isinstance(data, pd.Series):
-                _plot_values_above_bar(data, orient=orient, strfmt=strfmt, ax=ax)
+                self.label_bar_values(data, orient=orient, strfmt=strfmt, ax=ax)
             else:
                 for i, col in enumerate(data.columns):
-                    _plot_values_above_bar(data[col], orient=orient, strfmt=strfmt,
+                    self.label_bar_values(data[col], orient=orient, strfmt=strfmt,
                                           bar_nr=i, nr_bars=len(data.columns), ax=ax)
 
         elif plottype == 'bar_timeseries':
             ax = data.plot(kind='bar', color=color, **kwargs)
             if isinstance(data, pd.Series):
-                _plot_values_above_bar(data, orient=orient, strfmt=strfmt, ax=ax)
+                self.label_bar_values(data, orient=orient, strfmt=strfmt, ax=ax)
             else:
                 for i, col in enumerate(data.columns):
-                    _plot_values_above_bar(data[col], orient=orient, strfmt=strfmt,
+                    self.label_bar_values(data[col], orient=orient, strfmt=strfmt,
                                           bar_nr=i, nr_bars=len(data.columns), ax=ax)
 
         elif plottype == 'waterfall':
             ax, data, blank = self.plot_waterfall(data, color=color, **kwargs)
-            _plot_values_above_bar((data+blank), data, ax=ax, strfmt=strfmt, orient=orient)
+            self.label_bar_values((data+blank), ax=ax, display_values=data, strfmt=strfmt, orient=orient)
         elif plottype == 'waterfall_buildup':
             ax, data, blank = self.plot_waterfall(data, color=color, buildup=True, **kwargs)
-            _plot_values_above_bar((data+blank), data, ax=ax, strfmt=strfmt, orient=orient)
-
+            self.label_bar_values((data+blank), ax=ax, display_values=data, strfmt=strfmt, orient=orient)
 
         elif plottype == 'line_timeseries':
             ax = data.plot(color=color, **kwargs)
 
         elif plottype == 'scatter':
-            x = data.iloc[:, 0]
-            y = data.iloc[:, 1]
+            x = data.columns[0]
+            y = data.columns[1]
             try:
-                size = data.iloc[:, 2]
+                size = pd.Series(np.squeeze(sklearn.preprocessing.StandardScaler().fit_transform(data.iloc[:, [2]]) * 2)).add(20).clip(1)
             except IndexError:
                 size = None
-            ax = sns.scatterplot(x=x, y=y, size=size, color='grey', **kwargs)
-            for i, xi, yi in zip(data.index, x, y):
-                ax.annotate(i, (xi, yi))
+            
+            ax = data.plot(kind='scatter', x=x, y=y, s=size, c=color, **kwargs)
+            if len(data) <= 50:
+                for i, xi, yi in zip(data.index, x, y):
+                    ax.annotate(i, (xi, yi))
 
         elif plottype == 'composition_comparison':
             ax = data.transpose().plot(kind='barh', stacked=True, color=color, **kwargs)
@@ -188,26 +198,35 @@ class Consultant:
             if not isinstance(highlight, int):
                 raise TypeError('Can only highlight one line in composition comparison')
 
-            _plot_values_above_bar(location.iloc[highlight, :], data.iloc[highlight, :],
-                                  textcolor=self.contrasting_text_color, orient=orient, strfmt=strfmt, ax=ax)
+            self.label_bar_values(location.iloc[highlight, :], ax=ax, display_values=data.iloc[highlight, :],
+                                  textcolor=self.contrasting_text_color, orient=orient, strfmt=strfmt, text_alignment='center')
 
+        elif plottype == 'profile':
+            ax = self.plot_profile_comparison(data)
         elif plottype == 'piechart':
             raise TypeError('A piechart? Are you kidding me?')
         else:
             raise NotImplementedError(f'plottype {plottype} not available, choose "bar", "waterfall", "waterfall_buildup"'
-                                     ', "bar_timeseries", "line_timeseries", "scatter" or "composition_comparison"')
+                                     ', "bar_timeseries", "line_timeseries", "scatter", "profile" or "composition_comparison"')
         
-        # MAKE UP AXIS
-        ax.set_frame_on(False)
-        if orient == 'v' and plottype != 'scatter':
-            plt.xticks([])
-        elif orient == 'h' and plottype not in  ['line_timeseries', 'scatter']:
-            plt.yticks([])
+        try:
+            ax = self.make_up_axis(ax, orient, plottype)
+        except AttributeError:
+            ax = [self.make_up_axis(ax, orient, plottype='bar') for ax in ax]
         return ax
     
-    def plot_profile(self, data: pd.DataFrame, absolute=False, **kwargs):
+    def make_up_axis(self, ax, orient, plottype):
+        ax.set_frame_on(False)
+            
+        if orient == 'v' and plottype != 'scatter':
+            ax.set_xticks([])
+        elif orient == 'h' and plottype not in  ['line_timeseries', 'scatter']:
+            ax.set_yticks([])
+        return ax
+    
+    def plot_profile_comparison(self, data: pd.DataFrame, absolute=False, **kwargs):
         '''
-        Plot a profile
+        Plot a profile comparison, with a subset compared to the full set
 
         Parameters
         ----------
@@ -259,6 +278,7 @@ class Consultant:
         data: the data, including a "total"-row
         blank: the size of the blank space before each bar
         '''
+        #TODO: add connecting lines
         if color is None:
             color = ['lightgray'] * len(data)
 
@@ -276,77 +296,80 @@ class Consultant:
         ax = data.plot(kind='barh', stacked=True, left=blank, color=color, **kwargs)
         return ax, data, blank
     
-def _plot_values_above_bar(data, display_values=None, ax=None, strfmt='.2f', orient='v',
-                          textcolor='black', bar_nr=None, nr_bars=None):
-    '''
-    Helper function to display values above (or on) bar plot)
-    
-    Parameters
-    ----------
-    data: the series containing the position of the labels
-    display_values: Optionally: the values to display if different from data
-    ax: Axis object on which the labels should be plotted
-    strfmt: the format-string for the labels
-    orient: "v"(ertical) bars or "h"(orizontal) bars
-    bar_nr: if the plot contains multiple bar charts, which of the series we want to label
-    nr_bars: if the plot contains multiple bar charts, how many series are shown in total
-    '''
-    if (bar_nr is None) != (nr_bars is None):
-        raise ValueError('Either both bar_nr and nr_bars should be None, or neither')
-    
-    display_values = data if display_values is None else display_values
-    if ax is None:
-        ax = plt.gca()
-    
-    if orient == 'v':
-        lim_min, lim_max = ax.get_xlim()
-    elif orient == 'h':
-        lim_min, lim_max = ax.get_ylim()
-    else:
-        raise ValueError(f'orient must be "v" or "h", not {orient}')
+    @staticmethod
+    def label_bar_values(data, ax, display_values=None, strfmt='.2f', orient='v',
+                              textcolor='black', text_alignment=None, bar_nr=None, nr_bars=None):
+        '''
+        Helper function to display values above (or on) bar plot)
 
-    plotsize = lim_max - lim_min 
-    for i, (v, dv) in enumerate(zip(data, display_values)):
-        # offset label little bit
-        offset = 0.025 * plotsize
-        
-        if v < 0:
-            v -= offset
-            
-            if orient == 'v':
-                ha = 'right'
-            else:
-                va = 'top'
+        Parameters
+        ----------
+        data: the series containing the position of the labels
+        ax: Axis object on which the labels should be plotted
+        display_values: Optionally: the values to display if different from data
+        strfmt: the format-string for the labels
+        orient: "v"(ertical) bars or "h"(orizontal) bars
+        text_alignment: possible to overwrite alignment, using `https://matplotlib.org/3.1.1/tutorials/text/text_props.html`
+        bar_nr: if the plot contains multiple bar charts, which of the series we want to label
+        nr_bars: if the plot contains multiple bar charts, how many series are shown in total
+        '''
+        if (bar_nr is None) != (nr_bars is None):
+            raise ValueError('Either both bar_nr and nr_bars should be None, or neither')
+
+        if display_values is None:
+            display_values = data
+
+        if orient == 'v':
+            lim_min, lim_max = ax.get_xlim()
+        elif orient == 'h':
+            lim_min, lim_max = ax.get_ylim()
         else:
-            v += offset
-            if orient == 'v':
-                ha = 'left'
+            raise ValueError(f'orient must be "v" or "h", not {orient}')
+        plotsize = lim_max - lim_min 
+        offset = 0.025 * plotsize
+
+        for i, (v, dv) in enumerate(zip(data, display_values)):        
+            if v < 0:
+                v -= offset
+                if orient == 'v':
+                    ha = text_alignment or 'right'
+                    va = 'center'
+                else:
+                    va = text_alignment or 'top'
+                    ha = 'center'
             else:
-                va = 'bottom'
-        
-        if nr_bars is not None:
-            i += -0.5 + 1/(nr_bars+2) * (bar_nr + 1)
-        
-        x = v if orient == 'v' else i
-        y = i if orient == 'v' else v
-        label = '{:{prec}}'.format(dv, prec=strfmt)
-        ax.text(x, y, label, color=textcolor, va=va, ha=ha)
+                v += offset
+                if orient == 'v':
+                    ha = text_alignment or 'left'
+                    va = 'center'
+                else:
+                    va = text_alignment or 'bottom'
+                    ha = 'center'
 
-def _is_percentage_series(series):
-    '''
-    Checks whether a series contains all percentages
-    
-    By checking whether all values are between 0 and 1, and the sum is equal to 1
-    '''
-    return series.between(0, 1).all() and math.isclose(series.sum(), 1)
+            # Multiple bars become smaller: make sure the numbers align
+            if nr_bars is not None:
+                i += -0.5 + (bar_nr + 1) / (nr_bars+2)
 
+            x = v if orient == 'v' else i
+            y = i if orient == 'v' else v
+            label = '{:{prec}}'.format(dv, prec=strfmt)
+            ax.text(x, y, label, color=textcolor, va=va, ha=ha)
 
+    @staticmethod
+    def _is_percentage_series(series):
+        '''
+        Checks whether a series contains all percentages
 
-def _return_contrasting_text_color(colorname):
-        red, green, blue, alpha = colors.to_rgba(colorname)
+        By checking whether all values are between 0 and 1, and the sum is equal to 1
+        '''
+        return series.between(0, 1).all() and math.isclose(series.sum(), 1)
+
+    @staticmethod
+    def _return_contrasting_text_color(colorname):
+        '''
+        Calculates whether text on top of this color should be in black or white    
+        '''
+        red, green, blue, alpha = matplotlib.colors.to_rgba(colorname)
         if (red*0.299 + green*0.587 + blue*0.114) > 0.6:
             return 'black'
         return 'white'
-
-
-
